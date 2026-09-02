0570d825f0e1c5d3993eece87491e5cd9e1aff7d
Kousik27-23 <kousikghadai@gmail.com>
2026-09-02 19:01:39 +0530

    Time: 6 ms (33.34%) | Memory: 45.2 MB (35.73%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,53 +1,50 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
-
-        //using map
+        //1st approach(Using map)
         // Map<Integer,Integer> map =new HashMap<>();
 
         // for(int num:nums1){
         //     map.put(num,map.getOrDefault(num,0)+1);
-
         // }
 
-        // int res []=new int [nums1.length];
+        // int res[]=new int [nums1.length];
         // int k=0;
 
         // for(int num:nums2){
-        //     int count = map.getOrDefault(num,0);
-
+        //     int count=map.getOrDefault(num,0);
         //     if(count==0){
         //         continue;
-        //     }else{
+        //     }
+        //     else{
         //         res[k]=num;
         //         k++;
         //         map.put(num,count-1);
         //     }
-
         // }
         // return Arrays.copyOfRange(res,0,k);
 
 
+        //2nd Approach
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
-        int res []=new int[nums1.length];
+        int res[]=new int[nums1.length];
+        int k=0,i=0,j=0;
 
-        int i=0,j=0;
-        int k=0;
-
-       while(i<nums1.length && j<nums2.length){
-        if(nums1[i]==nums2[j]){
-            res[k]=nums1[i];
-            k++;
-            j++;
-            i++;
-        }else if(nums1[i]>nums2[j]){
-            j++;
-        }else{
-            i++;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                res[k]=nums1[i];
+                k++;
+                j++;
+                i++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
         }
-       }
         return Arrays.copyOfRange(res,0,k);
+        
     }
 }
