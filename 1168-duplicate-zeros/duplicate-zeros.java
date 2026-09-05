7ffabeb3cef394db504434aa709ee7825cbae9bc
Kousik27-23 <kousikghadai@gmail.com>
2026-09-05 21:53:40 +0530

    Time: 1 ms (99.42%) | Memory: 46.7 MB (89.79%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -3,33 +3,61 @@ class Solution {
 
         //1st Approach
 
-        int dest[]=new int[arr.length];
-        int s=0,d=0;
+        // int dest[]=new int[arr.length];
+        // int s=0,d=0;
 
-        while(s<arr.length){
-            if(arr[s]==0){
-                if(d<arr.length){
-                     dest[d]=0;
-                }
+        // while(s<arr.length){
+        //     if(arr[s]==0){
+        //         if(d<arr.length){
+        //              dest[d]=0;
+        //         }
                
-                d++;
-                if(d<arr.length){
-                     dest[d]=0;
-                }
+        //         d++;
+        //         if(d<arr.length){
+        //              dest[d]=0;
+        //         }
                 
-            }else{
-                if(d<arr.length){
-                    dest[d]=arr[s];
-                }
+        //     }else{
+        //         if(d<arr.length){
+        //             dest[d]=arr[s];
+        //         }
                 
+        //     }
+        //     d++;
+        //     s++;
+        // }
+        // for(int i=0;i<arr.length;i++){
+        //    arr[i]=dest[i];
+        // }
+
+        //2nd approach
+
+        int possibleZeroDups=0;
+        int lastIdx=arr.length-1;
+
+        for(int i=0;i<=lastIdx-possibleZeroDups;i++){
+            if(arr[i]==0){
+            //edge case
+            if(i==lastIdx-possibleZeroDups){
+                arr[lastIdx]=0;
+                lastIdx--;
+                break;
             }
-            d++;
-            s++;
+
+            possibleZeroDups++;
+
         }
-        for(int i=0;i<arr.length;i++){
-           arr[i]=dest[i];
         }
-        
-        
+    int newLastIdx = lastIdx-possibleZeroDups;
+    for(int i=newLastIdx;i>=0;i--){
+        if(arr[i]==0){
+            arr[i+possibleZeroDups]=0;
+            possibleZeroDups--;
+            arr[i+possibleZeroDups]=0;
+
+        }else{
+            arr[i+possibleZeroDups]=arr[i];
+        }
+    }
     }
 }
