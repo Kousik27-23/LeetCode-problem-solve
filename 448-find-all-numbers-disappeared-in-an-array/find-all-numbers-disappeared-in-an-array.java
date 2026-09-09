91bffb6a890359d1e2acbcc7b9bf49b10655c12c
Kousik27-23 <kousikghadai@gmail.com>
2026-09-09 23:07:56 +0530

    Time: 6 ms (84.78%) | Memory: 67 MB (73.69%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -3,17 +3,36 @@ class Solution {
 
         //1st Approach
         
-        boolean [] seen =new boolean[nums.length +1];
+        // boolean [] seen =new boolean[nums.length +1];
 
-        List<Integer> result =new ArrayList<>();
+        // List<Integer> result =new ArrayList<>();
 
-        for(int num:nums){
-            seen[num]=true;
+        // for(int num:nums){
+        //     seen[num]=true;
+        // }
+
+        // for(int i=1;i<=nums.length;i++){
+        //     if(!seen[i]){
+        //         result.add(i);
+        //     }
+        // }
+        // return result;
+
+        //2nd Approach (Optimize approach)
+
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i]) - 1;
+
+            if(nums[index]>0){
+                nums[index]= -nums[index];
+            }
         }
 
-        for(int i=1;i<=nums.length;i++){
-            if(!seen[i]){
-                result.add(i);
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                result.add(i + 1);
             }
         }
         return result;
